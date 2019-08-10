6895e91565779ee35164d4aec1aeedd118a62e4a
Paul <liquidream@hotmail.co.uk>
2019-08-10 16:16:01 +0100

    Added Patreon credits

--- ui_input.lua
+++ ui_input.lua
@@ -30,11 +30,57 @@ Navigate the path to safety, with the...
 **Music & SFX**:  
 **[Jason Riggs](https://twitter.com/_castlegames)**
 
-**Font**:  
+**Creative Director**:  
+**[Hilda Nicholas](https://twitter.com/shade_green)**
+
+**"Particle" Font**:  
 **[Eeve Somepx](https://twitter.com/somepx)**
+
+#### Special Thanks to the following supporters on [Patreon](https://www.patreon.com/liquidream):
+**•ThatTomHall•**  
+**•Roy Fielding•**  
+**•bbsamurai•**  
+**•Christopher Castillo•**  
+**•Not Invader Zim•**  
+**•Andy•**  
+Qristy Overton  
+Tobias V. Langhoff  
+Damien Cirade  
+Antonio Bianchetti  
+Joseph White  
+**•Graham Wenz•**  
+**•Daljit Chandi•**  
+**•Scramp Shortfriend•**  
+**•Vitorio Miliano•**  
+**•Andy Kaye (Anakey)•**  
+Gruber  
+Michael Leonardi  
+Paul Zimmermann  
+Hyperlink Your Heart  
+Llewelyn Griffiths  
+**•Andrew Dicker•**  
+**•Wilman•**  
+Joe  
+Johnathan Roatch  
+Morten Schouenborg  
+matteusbeus  
+Mike Poole  
 ]])
 
   end)
+
+  ui.section("Settings", function()
+    ui.markdown([[
+#### Reset Player Progress
+_**WARNING**: Clicking this will lose your saved progress!_
+]])
+    if ui.button("Reset", { kind='danger'}) then
+      -- reset progress
+      log("TODO: reset player progress..")
+    end
+
+  end)
+    
 
   -- ui.section("Patreon Thanks", function()
 
